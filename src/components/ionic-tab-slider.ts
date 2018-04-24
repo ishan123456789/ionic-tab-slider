bd6982ec095e2a96acf98ed4abb98c8981422892
75way Mac Mini <75way@75ways-Mac-mini-2.local>
2018-04-24 17:45:43 +0530

    Edited packages and html bugs

--- src/components/ionic-tab-slider.ts
+++ src/components/ionic-tab-slider.ts
@@ -1,10 +1,31 @@
 import {Component,ViewChild,Input} from '@angular/core';
 import { Slides } from 'ionic-angular';
 import { AfterViewInit } from '@angular/core';
-
+const templateToShow = `
+<div padding>
+    <ion-segment [(ngModel)]="currentSegment" (ionChange)="segmentChanged($event)">
+        <ion-segment-button [value]="tab.name" class="is-dark" *ngFor="let tab of tabs;let i = index;">
+            {{tab.name}}
+        </ion-segment-button>
+    </ion-segment>
+</div>
+<ion-grid class="slider-content">
+    <ion-row>
+        <ion-slides (ionSlideDidChange)="slideChanged()">
+            <ion-slide *ngFor="let tab of tabs;let i = index;let last = last;">
+                <ion-list class="list">
+                    <ion-item class="margin-top item-home-restaurant">
+                        <img [src]="tab.img" alt="">
+                    </ion-item>
+                </ion-list>
+            </ion-slide>
+        </ion-slides>
+    </ion-row>
+</ion-grid>
+`;
 @Component({
   selector: 'ionic-tab-slider',
-  templateUrl:  'ionic-tab-slider.html'
+  template:  templateToShow
 })
 export class IonicTabSliderComponent  implements AfterViewInit  {
   @ViewChild(Slides) slides: Slides;
